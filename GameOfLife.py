ca1d5e4d12fee2fc921fcde0b15de4dfd9d59606
Maximiliano Afonso <mecsafonso@gmail.com>
2020-04-30 17:01:10 -0300

    .

--- GameOfLife.py
+++ GameOfLife.py
@@ -3,7 +3,7 @@ import numpy as np
 
 pygame.init()
 
-width, height = 1000, 1000
+width, height = 100, 100
 screen = pygame.display.set_mode((height, width))
 
 bg = 25, 25, 25
@@ -14,10 +14,37 @@ nxC, nyC = 25, 25
 dimCW = width / nxC 
 dimCH = height / nyC
 
+gameState = np.zeros((nxC, nyC))
+
+gameState[5,3] = 1
+gameState[5,4] = 1
 while True:
 
     for y in range(0, nxC):
         for x in range(0, nyC):
 
-            poly: [((x)     * dimCW,    y)]
-    pass
+            n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+                      gameState[(x) % nxC, (y - 1) % nyC] + \
+                      gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+                      gameState[(x - 1) % nxC, (y) % nyC] + \
+                      gameState[(x + 1) % nxC, (y) % nyC] + \
+                      gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+                      gameState[(x) % nxC, (y + 1) % nyC] + \
+                      gameState[(x + 1) % nxC, (y + 1) % nyC] 
+            
+            if gameState[x, y] == 0 and n_neigh == 3:
+                gameState[x, y] = 1
+            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh >3):
+                gameState[x, y] = 0
+            
+            poly: [((x)   * dimCW, y * dimCH),
+                   ((x+1) * dimCW, y * dimCH),
+                   ((x+1) * dimCW, (y+1) * dimCH),
+                   ((x)   * dimCW, (y+1) * dimCH)]
+            
+            if newGameState[x, y] == 0:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+            else:
+                pygame.draw.polygon(screen, (255, 255, 255), poly, 0)
+            pygame.draw.polygon(screen,(128,128,128), poly,1)
+    
